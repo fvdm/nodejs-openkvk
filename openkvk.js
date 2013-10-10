b6af810ed2141a2ee2c6329380e40964e483c6c7
Franklin van de Meent <fr@nkl.in>
2013-10-10 03:46:06 +0200

    Added support for 'officieel' and 'api' endpoints
    - Simple requests on company name
    - Advanced SQL queries in database

--- openkvk.js
+++ openkvk.js
@@ -1,6 +1,33 @@
 var http = require('http')
 
 module.exports = function( query, callback ) {
+	if( typeof query === 'string' && !!~query.indexOf(' ') ) {
+		query += query.substring(-1) !== ';' ? ';' : ''
+		talk( 'api', query, function( err, data ) {
+			if( !err ) {
+				// data structure
+				var result = []
+				var props = data[0].RESULT.HEADER
+				
+				for( var r in data[0].RESULT.ROWS ) {
+					var row = data[0].RESULT.ROWS[r]
+					var record = {}
+					for( var i in row ) {
+						record[ props[i] ] = row[i]
+					}
+					result.push( record )
+				}
+				callback( null, result )
+			} else {
+				callback( err )
+			}
+		})
+	} else {
+		talk( 'officieel', query, callback )
+	}
+}
+
+function talk( host, query, callback ) {
 	// prevent multiple callbacks
 	var complete = false
 	function doCallback( err, res ) {
@@ -11,18 +38,15 @@ module.exports = function( query, callback ) {
 	}
 	
 	// build request
-	if( query.substring(-1) !== ';' ) {
-		query += ';'
-	}
-	
 	query = encodeURIComponent( query )
 	var options = {
-		host: 'api.openkvk.nl',
+		host: host +'.openkvk.nl',
 		path: '/json/'+ query,
 		header: {
 			'User-Agent': 'openkvk.js (https://github.com/fvdm/nodejs-openkvk'
 		}
 	}
+	
 	
 	var request = http.request( options )
 	
@@ -59,30 +83,8 @@ module.exports = function( query, callback ) {
 			
 			data = JSON.parse( data )
 			
-			// clean up
-			delete buf
-			delete pos
-			delete size
-			
-			// data structure
-			var result = []
-			var props = data[0].RESULT.HEADER
-			
-			for( var r in data[0].RESULT.ROWS ) {
-				var row = data[0].RESULT.ROWS[r]
-				var record = {}
-				for( var i in row ) {
-					record[ props[i] ] = row[i]
-				}
-				result.push( record )
-			}
-			
-			// clean up
-			delete result
-			delete props
-			
 			// done
-			doCallback( null, result )
+			doCallback( null, data )
 		})
 	})
 	
