202bc6db36eb4c52df92c37eb1ba53e18e3c0e89
Franklin van de Meent <fr@nkl.in>
2013-10-10 04:20:21 +0200

    Added the tests

--- test.js
+++ test.js
@@ -19,3 +19,30 @@ process.on( 'exit', function() {
 	}
 })
 
+// query
+console.log( 'Query on api.openkvk.nl:' )
+console.log( '  SELECT * FROM kvk WHERE bedrijfsnaam LIKE \'Ahold%\' LIMIT 1' )
+
+openkvk( 'SELECT * FROM kvk WHERE bedrijfsnaam LIKE \'Ahold%\' LIMIT 1', function( err, res ) {
+	if( err ) {
+		console.log( err, err.stack )
+		errors++
+	} else if( res[0].bedrijfsnaam.substring(0,5) !== 'Ahold' ) {
+		console.log( 'res[0].bedrijfsnaam.substring(0,5) !== \'Ahold\'' )
+		errors++
+	}
+})
+
+// simple
+console.log( 'Lookup on officieel.openkvk.nl:' )
+console.log( '  ahold' )
+
+openkvk( 'ahold', function( err, res ) {
+	if( err ) {
+		console.log( err, err.stack )
+		errors++
+	} else if( !~res[0].rechtspersoon.indexOf('Ahold') ) {
+		console.log( '!~res[0].rechtspersoon.indexOf(\'Ahold\')' )
+		errors++
+	}
+})
